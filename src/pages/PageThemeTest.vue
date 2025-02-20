a55d68de1164c6f6985b1335db715dff40f710b1
Stanley Ndagi <ndagis@gmail.com>
2025-02-21 01:12:41 +0300

    resource: update page names, add hidden /theme page

---
+++ src/pages/PageThemeTest.vue
@@ -0,0 +1,129 @@
+<template>
+  <q-page>
+    <div class="q-pa-md row items-start q-gutter-md">
+      <q-card
+        flat
+        bordered
+        class="my-card"
+        id="secondary"
+      >
+        <q-card-section>
+          <div class="text-h6">Secondary</div>
+        </q-card-section>
+
+        <q-card-section class="q-pt-none">
+          {{ lorem }}
+        </q-card-section>
+      </q-card>
+      <q-card
+        flat
+        bordered
+        class="my-card"
+        id="dark"
+      >
+        <q-card-section>
+          <div class="text-h6">Dark</div>
+        </q-card-section>
+
+        <q-card-section class="q-pt-none">
+          {{ lorem }}
+        </q-card-section>
+      </q-card>
+
+      <q-card
+        flat
+        bordered
+        class="my-card"
+        id="positive"
+      >
+        <q-card-section>
+          <div class="text-h6">Positive</div>
+        </q-card-section>
+
+        <q-card-section class="q-pt-none">
+          {{ lorem }}
+        </q-card-section>
+      </q-card>
+
+      <q-card
+        flat
+        bordered
+        class="my-card"
+        id="negative"
+      >
+        <q-card-section>
+          <div class="text-h6">Negative</div>
+        </q-card-section>
+
+        <q-card-section class="q-pt-none">
+          {{ lorem }}
+        </q-card-section>
+      </q-card>
+
+      <q-card
+        flat
+        bordered
+        class="my-card"
+        id="info"
+      >
+        <q-card-section>
+          <div class="text-h6">Info</div>
+        </q-card-section>
+
+        <q-card-section class="q-pt-none">
+          {{ lorem }}
+        </q-card-section>
+      </q-card>
+
+      <q-card
+        flat
+        bordered
+        class="my-card"
+        id="warning"
+      >
+        <q-card-section>
+          <div class="text-h6">Warning</div>
+        </q-card-section>
+
+        <q-card-section class="q-pt-none">
+          {{ lorem }}
+        </q-card-section>
+      </q-card>
+    </div>
+  </q-page>
+</template>
+
+<script setup>
+const lorem = 'Lorem ipsum dolor sit amet, consectetur adipiscing elit'
+</script>
+
+<style lang="scss" scoped>
+.my-card {
+  width: 100%;
+  max-width: 250px;
+}
+
+#secondary {
+  background-color: $secondary;
+}
+
+#dark {
+  background-color: $dark;
+}
+
+#positive {
+  background-color: $positive;
+}
+
+#negative {
+  background-color: $negative;
+}
+
+#info {
+  background-color: $info;
+}
+
+#warning {
+  background-color: $warning;
+}
+</style>
--- src/router/routes.js
+++ src/router/routes.js
@@ -3,8 +3,9 @@ const routes = [
     path: '/',
     component: () => import('layouts/MainLayout.vue'),
     children: [
-      { path: '', component: () => import('pages/EntriesPage.vue') },
-      { path: 'settings', component: () => import('pages/SettingsPage.vue') }
+      { path: '', component: () => import('pages/PageEntries.vue') },
+      { path: 'settings', component: () => import('pages/PageSettings.vue') },
+      { path: 'theme', component: () => import('pages/PageThemeTest.vue') }
     ]
   },
 
